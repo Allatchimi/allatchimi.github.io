d5258153bcd15f8798e901acf70e57f5f41de174
Allatchimi <kellanamine@gmail.com>
2025-04-09 00:49:10 +0100

    change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/', // OK si ton repo est allatchimi.github.io
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+
+  ],
 })
 
 
